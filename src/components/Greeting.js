0be96ec42ce233a831fdbb8881115b3e9c1163f3
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-12-04 11:33:57 -0700

    testing user interaction & state

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,8 +1,20 @@
+import {useState} from "react"
+
+
+
 const Greeting = () => {
+    const [changedText, setChangedText] = useState(false);
+
+    const changedTextHandler = () => {
+        setChangedText(true);
+    }
+
     return(
         <div>
             <h2>HELLO WORLD</h2>
-            <p> ITS GOOD TO SEE YOU</p>
+          { !changedText&& <p> ITS GOOD TO SEE YOU</p>}
+          { changedText &&  <p>CHANGED!</p>}
+            <button onClick={changedTextHandler}>CHANGE TEXT</button>
         </div>
     )
 }
--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting"
 
 describe("Greeting component", () => {
@@ -13,5 +14,30 @@ describe("Greeting component", () => {
     const helloWorldElement = screen.getByText('HELLO WORLD')
     expect( helloWorldElement).toBeInTheDocument()
     });
+
+test("renders good to see you if the buttons was not clicked", () => {
+    render(<Greeting/>);
+
+    const outputElement = screen.getAllByText('good to see you', {exact: false})
+    expect(outputElement).toBeInTheDocument();
+});
+
+test('renders changed! if the button was clicked', () => {
+    //Arrange
+    render(<Greeting/>)
+    
+    //Act
+    const buttonElement = screen.getByRole('button')
+userEvent.click(buttonElement)
+
+//Assert
+
+const outputElement = screen.queryByText('good to see you', {exact: false})
+expect(outputElement).toBeNull();
+
+});
+
+
+
 });
 
