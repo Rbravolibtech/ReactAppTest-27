494e828b87f19da27c4632697aa9c9a72b7ce6ea
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-12-04 10:54:25 -0700

    grouping test together with test suites

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,15 +1,17 @@
 import { render, screen } from "@testing-library/react"
 import Greeting from "./Greeting"
 
+describe("Greeting component", () => {
+    test('renders HELLO WORLD AS A TEXT', () => {
+        //Arrange
+        render (<Greeting/>);
+    
+        //Act
+        //..nothing
+    
+        //Assert
+    const helloWorldElement = screen.getByText('HELLO WORLD')
+    expect( helloWorldElement).toBeInTheDocument()
+    });
+});
 
-test('renders HELLO WORLD AS A TEXT', () => {
-    //Arrange
-    render (<Greeting/>);
-
-    //Act
-    //..nothing
-
-    //Assert
-const helloWorldElement = screen.getByText('HELLO WORLD')
-expect( helloWorldElement).toBeInTheDocument()
-})
